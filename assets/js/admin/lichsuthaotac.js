f4f3d7b8f46a0075c03ce276e51ac2e5e0a0eac0
giangan-ng <nganng0751@ut.edu.vn>
2025-11-16 02:55:34 +0700

    update lichsuthaotac

--- assets/js/admin/lichsuthaotac.js
+++ assets/js/admin/lichsuthaotac.js
@@ -1,2 +1,6 @@
-// lichsuthaotac.js
-// File này hiện để trống
+// lichsuthaotac_admin.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Nơi để viết các sự kiện cho trang lịch sử
+    // Ví dụ: code cho bộ lọc ngày tháng, v.v.
+});
